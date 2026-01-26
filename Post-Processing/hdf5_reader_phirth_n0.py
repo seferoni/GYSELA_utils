67382e95a49b3483b0f972162ff3e45d001c6c81
Asim Seth <131764979+seferoni@users.noreply.github.com>
2026-01-26 14:30:13 +0800

    HDF5 reader logic refinement.

---
+++ Post-Processing/hdf5_reader_phirth_n0.py
@@ -0,0 +1,55 @@
+import sys;
+import h5py;
+import numpy as np;
+import glob;
+from pathlib import Path;
+
+# Auxiliary functions.
+def compile_phi2D_data(h5_files):
+
+	# Sequence/list of 2D arrays, each array a snapshot of the electrostatic potential.
+	phi2D_data = [];
+
+	for h5_file in h5_files:
+		phi2D_n0 = read_hdf5_file(h5_file);
+		phi2D_data.append(phi2D_n0);
+
+	return np.array(phi2D_data);
+
+def fetch_phi2D_filepaths(nominal_path):
+
+	directory_path = Path(nominal_path);
+
+	if not directory_path.is_dir(directory_path):
+		print(f"Error: The given directory '{nominal_path}' could not be resolved.");
+		return [];
+
+	h5_files = [file.resolve() for file in directory_path.glob("Phi2D_d*.h5")];
+	return sorted(h5_files);
+
+def read_hdf5_file(filepath):
+
+	h5_file = h5py.File(filepath, 'r')
+	print(f"{filepath} has been successfully resolved and opened.");
+	return h5_file["Phirth_n0"][:];
+
+# Main script logic.
+def main():
+
+	if len(sys.argv) != 2:
+		print("Usage: python hdf5_reader.py <absolute_path_to_hdf5_file>");
+		print("Aborting.");
+		sys.exit(1);
+
+	h5_directory = sys.argv[1];
+	h5_files = fetch_phi2D_filepaths(h5_directory);
+	
+	if len(h5_files) == 0:
+		print("No HDF5 files found in the specified directory.");
+		print("Aborting.");
+		sys.exit(1);
+
+	phi2D_data = compile_phi2D_data(h5_files);
+	# TODO: logic here
+
+main();
