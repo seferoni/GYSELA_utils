5133619d6a27d23ffde9f75ad76cbce858598ec8
Asim Seth <131764979+seferoni@users.noreply.github.com>
2026-04-25 12:23:03 +0800

    Jacobian initial impl.

--- post-processing/scripts/gysela_utilities.py
+++ post-processing/scripts/gysela_utilities.py
@@ -17,3 +17,13 @@ def generate_time_range_by_series(radial_time_series, dt_diag):
 
 	naive_range = np.arange(len(radial_time_series));
 	return naive_range * dt_diag;
+
+def flux_surface_average_2D(quantity_xarray, jacobian_dictionary):
+	# TODO: untested
+	return (quantity_xarray * jacobian_dictionary["naive"]).sum(dim = ["theta"]) / jacobian_dictionary["intdtheta_Js"];
+
+def flux_surface_average_3D(quantity_xarray, jacobian_dictionary):
+	# TODO: untested
+	return (quantity_xarray * jacobian_dictionary["naive"]).sum(dim = ["theta", "phi"]) / jacobian_dictionary["intdthetadphi_Js"];
+
+# TODO: don't forget the radial averages
